93cfae39d37ac871c7a1dd29c7e8112948e81e4b
alanasrocha08 <alanasr02@gmail.com>
2025-03-14 10:55:15 -0300

    try/catch

--- conecta.php
+++ conecta.php
@@ -7,9 +7,21 @@ $banco = "vendas";
 
 /* Configurações para conexão ao banco de dados */
 
-// Criando conexão com o banco usando a classe PDO
+/*Bloco try/catch 
+Passamos no try tudo que queremos que seja feito (as ações de confi/conexão com o BD) */
+try {
+    // Criando conexão com o banco usando a classe PDO
 // PDO (PDO PHP Data Object): Classe para manipulação de banco de dados
 $conexao = new PDO(
     "mysql:host=$servidor;dbname=$banco;charset=utf8",
     $usuaro, $senha 
 );
+/* Configurar PDO para lançar exceções/erros caso ocorram */
+$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (\Throwable $th) {
+    /*E colocamos no catch o que fazer CASO alguma ação no try FALHE. Neste caso, é gerada uma execeção/erro e exibimos a mensaem sobre ela */
+    die("Deu ruim: ".$erro->getMessage() );
+}
+
+
