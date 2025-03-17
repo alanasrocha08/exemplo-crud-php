dcc32d7ba6bab814a7f24c04b841a63937ede42f
alanasrocha08 <alanasr02@gmail.com>
2025-03-17 11:58:48 -0300

    começa exercício

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -53,3 +53,17 @@ function listarUmFabricante(PDO $conexao, int $idFabricante):array {
     }
 }
 
+//atualizarFabricante: usada em fabricantes/atualizar.php
+function atualizarFabricante($conexao, $idFabricante, $nomeDoFabricante) :void {
+    $sql = "UPDATE fabricantes SET nome = :nome WHERE id = :id";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":nome", $conexao, PDO::PARAM_STR);
+        $consulta->bindValue(":id", $conexao, PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao atualizar fabricante: " . $erro->getMessage());
+    }
+}
+
