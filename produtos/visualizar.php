c718d5a674d76108c5a954f667d860e561a0a40c
alanasrocha08 <alanasr02@gmail.com>
2025-03-25 11:51:47 -0300

    Adiciona foreach

--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -22,6 +22,19 @@ $listaDeProdutos = listarProdutos($conexao);
 
         <p><a class="btn btn-primary btn-sm" href="inserir.php">Inserir novo produto</a></p>
 
+        <div class="row g-1">
+            
+    <?php foreach ($listaDeProdutos as $produto) : ?>
+        <div class="col-sm-6">
+            <article class="bg-body-secondary p-2">
+                <h3><?=$produto["nome"] ?> </h3> 
+                <h4><?=$produto["fabricante_id"]?></h4>
+                <p><b>Preço:</b> <?=$produto["preco"]?></p>
+                <p><b>Quantidade: </b> <?=$produto["quantidade"]?></p> 
+            </article>
+        </div> <!-- Correção da tag div -->
+    <?php endforeach; ?>
+</div>
 
     </div>
 
