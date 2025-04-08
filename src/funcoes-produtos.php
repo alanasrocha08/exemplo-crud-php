1b09ba0a3b759e4eb14e53565bb32debf2cb7e7b
alanasrocha08 <alanasr02@gmail.com>
2025-04-08 08:40:14 -0300

    Atualiza

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -2,54 +2,56 @@
 require_once "conecta.php";
 
 function listarProdutos(PDO $conexao):array {
-    //$sql = "SELECT * FROM produtos";
+    // $sql = "SELECT * FROM produtos";
     $sql = "SELECT 
-        produtos.id, produtos.nome AS produto,
-        produtos.preco, produtos.quantidade,
-        fabricantes.nome AS fabricante
-        FROM produtos INNER JOIN fabricantes
-        ON produtos.fabricante_id = fabricantes.id
-        ORDER BY produto";
+                produtos.id, produtos.nome AS produto, 
+                produtos.preco, produtos.quantidade,
+                (produtos.preco * produtos.quantidade) AS total,
+                fabricantes.nome AS fabricante
+            FROM produtos INNER JOIN fabricantes
+            ON produtos.fabricante_id = fabricantes.id
+            ORDER BY produto";
+
     try {
         $consulta = $conexao->prepare($sql);
         $consulta->execute();
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     } catch (Exception $erro) {
-        die("Erro ao carregar proodutos: ".$erro->getMessage());
+        die("Erro ao carregar produtos: ".$erro->getMessage());
     }
-};
+}
 
 
-function inserirProduto(
-    PDO $conexao, string $nome, float $preco, int $quantidade, int $fabricante, string $descricao):void {
+function inserirProduto( 
+    PDO $conexao, string $valorNome, float $valorPreco,
+    int $valorQuantidade, int $idFabricante, string $valorDescricao  
+    ):void {
 
         $sql = "INSERT INTO produtos(nome, descricao, preco, quantidade, fabricante_id)
-        VALUES(:nome, :descricao, :preco, :quantidade, :fabricante_id)";
+                VALUES(:nome, :descricao, :preco, :quantidade, :fabricante_id)";
+
     try {
         $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
-        $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
-        $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
-        $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
-        $consulta->bindValue(":fabricante_id", $fabricante, PDO::PARAM_INT);
+        $consulta->bindValue(":nome", $valorNome, PDO::PARAM_STR);
+        $consulta->bindValue(":descricao", $valorDescricao, PDO::PARAM_STR);
+        $consulta->bindValue(":preco", $valorPreco, PDO::PARAM_STR);
+        $consulta->bindValue(":quantidade", $valorQuantidade, PDO::PARAM_INT);
+        $consulta->bindValue(":fabricante_id", $idFabricante, PDO::PARAM_INT);
         $consulta->execute();
     } catch (Exception $erro) {
         die("Erro ao inserir produto: ".$erro->getMessage());
-    }
-};
-
-//listarUmProduto: usada pela página produtos/atualizar.php
+    }              
+}
 
 function listarUmProduto(PDO $conexao, int $idProduto):array {
     $sql = "SELECT * FROM produtos WHERE id = :id";
-
-    try{
+ 
+    try {
         $consulta = $conexao->prepare($sql);
         $consulta->bindValue(":id", $idProduto, PDO::PARAM_INT);
         $consulta->execute();
         return $consulta->fetch(PDO::FETCH_ASSOC);
     } catch (Exception $erro) {
-        die("Erro ao carregar fabricante: ".$erro->getMessage());
-    }
+        die("Erro ao carregar produto: ".$erro->getMessage());
+    }   
 }
-
